7b93a7c11dd9d4b517c8dd523b52aa30e7bceddd
Ragdata <ragdata@users.noreply.github.com>
2023-05-28 22:34:00 +1000

    feat(template): update base-alpine

--- test/base-alpine/test.sh
+++ test/base-alpine/test.sh
@@ -6,7 +6,7 @@ source ../test-utils/test-utils.sh
 
 PACKAGE_LIST="openssh-client gnupg procps lsof htop net-tools psmisc curl wget rsync ca-certificates unzip zip nano vim less jq \
 	libgcc libstdc++ krb5-libs libintl libssl1.1 lttng-ust tzdata userspace-rcu zlib sudo coreutils sed grep which ncdu shadow \
-	strace man-pages"
+	strace man-pages git"
 
 checkCommon "$PACKAGE_LIST"
 
